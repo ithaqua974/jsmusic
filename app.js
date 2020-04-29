60d380cc82517af711811acdadffa69873f972d9
ithaqua974 <tristan.hoarau@gmail.com>
2020-04-29 09:44:24 +0400

    server+ app require

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+// const mongoose = require('mongoose');
+
+const musicRoutes = require('./routes/music');
+const userRoutes = require('./routes/user');
+const path = require('path');
+
+
+// mongoose.connect('mongodb+srv://tristan:GAEtn78H974@cluster0-15ojg.mongodb.net/test?retryWrites=true&w=majority', {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//         useCreateIndex: true
+//     })
+//     .then(() => console.log('Connexion à MongoDB réussie !'))
+//     .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(bodyParser.json());
+
+app.use('/api/music', musicRoutes);
+app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+module.exports = app;
